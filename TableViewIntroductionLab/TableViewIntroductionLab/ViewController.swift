eec422d0cb44214c3d2b3c0ce30858d321b936b5
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-11-13 17:44:45 -0500

    TableView for task

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var tasks = [Task]()    {
+        didSet  {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numberOfTasks = Task.allTasks.count
+        return numberOfTasks
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        
+        let task = Task.allTasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        
+        return cell
+    }
+}
